5f994ac2cfd6c61063831649ae28fce11d8d1c9b
Gonçalo <goncaloscravo@gmail.com>
2023-10-26 14:54:22 +0100

    Add: a Kafka producer implementation

---
+++ src/main/java/gsc/projects/springbootkafka/kafka/KafkaProducer.java
@@ -0,0 +1,22 @@
+package gsc.projects.springbootkafka.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    private void sendMessage(String message){
+        LOGGER.info(String.format("Message sent %s", message));
+        kafkaTemplate.send("javaguides", message);
+    }
+}
